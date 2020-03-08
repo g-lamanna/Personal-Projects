e82fdaeb07fbb3ac2f723dc8098eb55f9181a127
g-lamanna <gabe.lamanna@outlook.com>
2020-03-07 21:55:39 -0500

    Adds Functionality to add user credentials to text file

--- passWord.py
+++ passWord.py
@@ -19,20 +19,54 @@ def password_introduction():
     -(R)andom Password
     ''')
     user_answer = input()
-    options = {"R":random_password_generator()
-             #  "A":add_credentials(),
-              # "S":search_password()
-                }
-    if user_answer == options[user_answer]:
-        options[user_answer]
+    if user_answer == "r":
+        random_password_generator()
+    elif user_answer == "a":
+        add()
+    else:
+        print("Please Try again")
+        password_introduction()
+    cont = ''
+    while cont !="N":
+        print("Is there anything else? ")
+        cont = input()
+        if cont == 'N':
+            break
+        else:
+            return password_introduction()
 def random_password_generator():
     import random
     characters = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_pass = ""
     for i in range(16):
         new_pass += random.choice(characters)
-    print (new_pass)
-def add_credentials():
+    print(new_pass)
+    return (new_pass)
+def add():
+    file = "/Users/gabriel/Documents/projects/pandora.txt"
+    with open(file,"a+") as file_container:
+        user_pass = []
+        print("Add Site/Service")
+        site = input()
+        print("Add Username ")
+        user_name = input()
+        print("Add Password")
+        password = input()
+        user_pass.append(site)
+        user_pass.append(user_name)
+        user_pass.append(password)
+        string_pass = str(user_pass).replace(",",":")
+        file_container.write(string_pass)
+        file_container.write('\n')
+        cont = ""
+        while cont != 'N':
+            print("Do you want to continue? 'No' to stop")
+            cont = input()
+            if cont == "N":
+                break
+            else:
+                add()
+                return user_pass
     
     
     
@@ -42,3 +76,4 @@ def add_credentials():
     
 
 passManager()
+
