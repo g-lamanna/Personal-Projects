dde56b7f3f6a934bfa9a863fc22fdec99b3655fa
g-lamanna <gabe.lamanna@outlook.com>
2020-10-08 17:02:34 -0400

    Added method to include attack options

--- pokemon.py
+++ pokemon.py
@@ -28,30 +28,76 @@ class Pokemon():
         if self.current_health < 0:
             self.current_health = 0
         if self.pokemon_status(self.current_health) == True:
-            return ("{} used water gun! It's super effective! {} taking {} in damage with {} left....{} fainted!".format(poke_X.name,poke_Y.name,self.opp_attack_power,self.current_health,poke_Y.name))
+            return ("It's super effective! {} taking {} in damage with {} left....{} fainted!".format(poke_Y.name,self.opp_attack_power,self.current_health,poke_Y.name))
         else:
-            return("{} taking {} in damage,still fighting with {} left!".format(poke_Y.name,self.opp_attack_power,self.current_health))
+            return("It's super effective! {} taking {} in damage with {} left".format(poke_Y.name,self.opp_attack_power,self.current_health))
 
     
     def poke_attack(self,poke_X,poke_Y):
-        poke_X_attack = 40
+        poke_X_attack = 20
         if poke_X.pok_type == "Water" and poke_Y.pok_type == "Fire":
             poke_X_attack += poke_X_attack * 0.40
+            self.attack_moves(poke_X,poke_X_attack)
         elif poke_X.pok_type == "Grass" and poke_Y.pok_type == "Water": 
             poke_X_attack += poke_X_attack * 0.40
+            self.attack_moves(poke_X,poke_X_attack)
         elif poke_X.pok_type == "Fire" and poke_Y.pok_type == "Grass":
             poke_X_attack += poke_X_attack * 0.40
+            self.attack_moves(poke_X,poke_X_attack)
         elif poke_X.pok_type == "Fire" and poke_Y.pok_type == "Water":
             poke_X_attack -= poke_X_attack * 0.40
+            self.attack_moves(poke_X,poke_X_attack)
+        elif poke_X.pok_type == "Water" and poke_Y.pok_type == "Grass":
+            poke_X_attack -= poke_X_attack * 0.40
+            self.attack_moves(poke_X,poke_X_attack)
+        elif poke_X.pok_type == "Grass" and poke_Y.pok_type == "Fire":
+            poke_X_attack -= poke_X_attack * 0.40
+            self.attack_moves(poke_X,poke_X_attack)
         
         return self.lose_health(poke_X_attack)
 
+    def attack_moves(self,poke_X,attack_power):
+        move = ""
+        if poke_X.pok_type == "Grass":
+            move = input("Select a move: 'Leaf Blade' or 'Tackle' ")
+            if move == "Leaf Blade":
+                attack_power += attack_power * 0.25
+                print("{} used {}!".format(poke_X.name,move))
+                return attack_power
+            else:
+                print("{} used {}!".format(poke_X.name,move))
+                return attack_power
+        elif poke_X.pok_type == "Water":
+            move = input("Select a move: 'Hydro Pump' or 'Pound' ")
+            if move == "Hydro Pump":
+                attack_power += attack_power * 0.25
+                print("{} used {}!".format(poke_X.name,move))
+                return attack_power
+            else:
+                print("{} used {}!".format(poke_X.name,move))
+                return attack_power
+        elif poke_X.pok_type == "Fire":
+            move = input("Select a move: 'Flame Thrower' or 'Scratch' ")
+            if move == "Flame Thrower":
+                attack_power += attack_power * 1
+                print("{} used {}!".format(poke_X.name,move))
+                return attack_power
+            else:
+                print("{} used {}!".format(poke_X.name,move))
+                return attack_power,move
 
 
+
+#ATTACKER
 poke_X = Pokemon("Squirtle",10,"Water",50,50,False)
-poke_Y = Pokemon("Charmander",10,"Fire",50,50,False)
 
-print(poke_X.poke_attack(poke_X,poke_Y))
+#DEFENDER
+poke_Y = Pokemon("Charizard",10,"Fire",50,50,False)
+
+poke_Z = Pokemon("Venasaur",10,"Grass",50,50,False)
+
+
+print(poke_X.poke_attack(poke_Y,poke_X))
 
 
 
